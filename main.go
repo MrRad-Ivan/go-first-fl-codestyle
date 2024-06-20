fadd16ef6df969b6e867983e909d8b1151b71488
Melashenko Ivan <melashenkoivan98@gmail.com>
2024-06-20 20:30:37 +0300

    code according to rules

--- main.go
+++ main.go
@@ -25,13 +25,16 @@ func attack(charName, charClass string) string {
 func defence(char_name, char_class string) string {
 	if char_class == "warrior" {
 		return fmt.Sprintf("%s блокировал %d урона.", char_name, 10+randint(5, 10))
-	} else if char_class == "mage" {
-		return fmt.Sprintf("%s блокировал %d урона.", char_name, 10+randint(-2, 2))
-	} else if char_class == "healer" {
-		return fmt.Sprintf("%s блокировал %d урона.", char_name, 10+randint(2, 5))
-	} else {
-		return "неизвестный класс персонажа"
 	}
+	if char_class == "mage" {
+		return fmt.Sprintf("%s блокировал %d урона.", char_name, 10+randint(-2, 2))
+	}
+	if char_class == "healer" {
+		return fmt.Sprintf("%s блокировал %d урона.", char_name, 10+randint(2, 5))
+	}
+
+	return "неизвестный класс персонажа"
+
 }
 
 // обратите внимание на "if else" и на "else"
